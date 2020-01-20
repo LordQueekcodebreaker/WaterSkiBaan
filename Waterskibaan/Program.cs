28716cbc8ed0340bc65ff3c8fa15cf65f392a43a
VimtoFive <joost.oltmans@gmail.com>
2020-01-20 14:04:58 +0100

    Updated Wachtrij

--- Waterskibaan/Program.cs
+++ Waterskibaan/Program.cs
@@ -11,7 +11,11 @@ namespace Waterskibaan
     {
         static void Main(string[] args)
         {
-            
+            Waterskibaan waterskibaan = new Waterskibaan();
+            for (int i = 0; i < 20; i++)
+            {
+                waterskibaan.SporterStart(new Sporter(MoveCollection.GetWillekeurigeMoves()) { Skies = new Skies(), Zwemvest = new Zwemvest()});
+            }
         }
     }
 }
